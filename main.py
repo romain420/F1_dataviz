624dcdd022b78894ae0148b6c5fa9f3b9c282116
romain240 <romain.dreuilhet@protonmail.com>
2021-11-02 15:23:19 +0100

    gestion installatio package data cleaning

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import subprocess
+import os
  
-subprocess.run(["python", "./F1_dataviz_last.py"])
+subprocess.run(["python", "./install_resouces.py"])
+
+if os.path.exists('merge_race_result.csv') and os.path.exists('best_team_pit_stop.csv') and os.path.exists('df_constructor_standings.csv') and os.path.exists('df_driver_standings.csv'):
+    print('file already exist')
+else:
+    print("lancement du processuce du nettoyage de données")
+    subprocess.run(["python", "./F1_dataviz_last.py"]) 
+    
 subprocess.run(["python", "./first_dash_test.py"])
